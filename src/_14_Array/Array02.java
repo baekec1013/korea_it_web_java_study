116f36b02908e61cd8853f7a18a2a59e3cef3dad
baekec1013 <baekec1013@gmail.com>
2025-05-20 20:46:06 +0900

    Feat: array01, array02

---
+++ src/_14_Array/Array02.java
@@ -0,0 +1,18 @@
+package _14_Array;
+
+public class Array02 {
+    public static void main(String[] args) {
+        // 배열 순회
+
+        String[] countries = {"Hungary", "Croatia", "Slovenia", "Austria", "Czech"};
+//        // 반복문 for문을 통해 배열 순회
+//        for (int i = 0; i< countries.length; i++) {
+//            System.out.println(countries[i]);
+
+
+        // enhanced for (for-each) 향상된 for문
+        for (String country : countries) {
+            System.out.println(country);
+        }
+    }
+}
